09864adaada432e16e7d9646f462a809b4b8c910
dumbcat <dumbcat0719@gmail.com>
2018-04-09 21:06:21 +0800

    change map link

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ from linebot.models import (
     TextSendMessage,
     ImageSendMessage
 )
-from google_sheet import gsheet
+# from google_sheet import gsheet
 from datetime import datetime
+import datetime as dt
 import re
 import json
 import schedule
@@ -33,6 +34,10 @@ handler = WebhookHandler('99f62b98d42e9be53921fa023b9bd754')
 group_list = ['C22815b8fb3667c8c87886dec9e862810',
               'C4b622b292c25070df8ff03b11e35e3e9'
               ]
+
+# 取得本周第一天的日期
+date1 = datetime.now()
+this_week_start_dt = str(date1 - dt.timedelta(days=date1.weekday())).split()[0]
 
 
 # 監聽所有來自 /callback 的 Post Request
@@ -65,27 +70,24 @@ def handle_message(event):
     # 回傳遺跡地圖的圖片訊息
     if re.match('^@\d\d\u907a\u8de1', event.message.text):
         # 取得google試算表遺跡地圖連結
-        values_list = gsheet()
+        # values_list = gsheet()
+        # https://ro.fws.tw/uploads/raid/2018-04-09/EG_2018-04-09_80.jpg
+        # message_error = TextSendMessage(text="抱歉，尚未有本周遺跡路線")
+        url = 'https://ro.fws.tw/uploads/raid/' + \
+            this_week_start_dt + '/EG_' + this_week_start_dt
+        if event.message.text == u"@40遺跡":
+            url = url + '_40.jpg'
+        if event.message.text == u"@60遺跡":
+            url = url + '_60.jpg'
+        if event.message.text == u"@80遺跡":
+            url = url + '_80.jpg'
 
-        message_error = TextSendMessage(text="抱歉，尚未有本周遺跡路線")
-
-        # 以週數戳記確認遺跡地圖是否更新
-        if datetime.now().isocalendar()[1] != int(values_list[6]):
-            line_bot_api.reply_message(event.reply_token, message_error)
-        else:
-            if event.message.text == u"@40遺跡":
-                map_no = 0
-            if event.message.text == u"@60遺跡":
-                map_no = 2
-            if event.message.text == u"@80遺跡":
-                map_no = 4
-
-            # 取得對應的遺跡地圖連結，儲存為回應訊息格式
-            message = ImageSendMessage(
-                original_content_url=values_list[map_no],
-                preview_image_url=values_list[map_no + 1]
-            )
-            line_bot_api.reply_message(event.reply_token, message)
+        # 取得對應的遺跡地圖連結，儲存為回應訊息格式
+        message = ImageSendMessage(
+            original_content_url=url,
+            preview_image_url=url
+        )
+        line_bot_api.reply_message(event.reply_token, message)
 
     # 回傳恩德勒斯塔Boss文字訊息
     if re.match('^@B.+', event.message.text):
